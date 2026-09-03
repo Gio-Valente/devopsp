5254fec5670f7f5e41301528ab674155e5797fd0
Gio-Valente <giovannacumin@gmail.com>
2026-09-03 11:18:31 -0300

    adiciona cadastro de tutores

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+tutores = []
 
 
 @app.route("/")
 def home():
-    return """
-    <h1>Cãotinho</h1>
-    <p>Sistema básico de gestão para creche de cães.</p>
+    return render_template("index.html", tutores=tutores)
 
-    <h2>Funcionalidades</h2>
 
-    <ul>
-        <li>Cadastro de au-lunos</li>
-        <li>Cadastro de tutores</li>
-    </ul>
-    """
+@app.route("/cadastrar-tutor", methods=["POST"])
+def cadastrar_tutor():
+    nome = request.form["nome"]
+    telefone = request.form["telefone"]
+    email = request.form["email"]
+
+    tutor = {
+        "nome": nome,
+        "telefone": telefone,
+        "email": email
+    }
+
+    tutores.append(tutor)
+
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
